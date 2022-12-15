cd3b24286df33a2e4fb63328458e4ff3e2683984
Jeff Hammond <jeff.science@gmail.com>
2022-12-15 09:25:49 +0200

    fix init thread stuff

--- src/init.c
+++ src/init.c
@@ -27,8 +27,9 @@ int MPI_Finalize(void)
 int MPI_Init_thread(int *argc, char ***argv, int required, int *provided)
 {
     int shmem_requested, shmem_provided;
+    int mpi_required, mpi_provided;
 
-    int rc = PMPI_Init_thread(argc, argv, required, provided);
+    int rc = PMPI_Init_thread(argc, argv, mpi_required, &mpi_provided);
     if (rc != MPI_SUCCESS) return rc;
 
     if (       required  ==   MPI_THREAD_SINGLE) {
@@ -44,14 +45,14 @@ int MPI_Init_thread(int *argc, char ***argv, int required, int *provided)
     int rx = shmem_init_thread(shmem_requested, &shmem_provided);
     if (rx != 0) return MPI_ERR_OTHER;
 
-    if (       shmem_provided == SHMEM_THREAD_SINGLE) {
-                     provided =    MPI_THREAD_SINGLE;
-    } else if (shmem_provided == SHMEM_THREAD_FUNNELED) {
-                     provided =    MPI_THREAD_FUNNELED;
-    } else if (shmem_provided == SHMEM_THREAD_SERIALIZED) {
-                     provided =    MPI_THREAD_SERIALIZED;
-    } else if (shmem_provided == SHMEM_THREAD_MULTIPLE) {
-                     provided =    MPI_THREAD_MULTIPLE;
+    if ((shmem_provided >= SHMEM_THREAD_MULTIPLE) && (mpi_provided >= MPI_THREAD_MULTIPLE)) {
+        *provided = MPI_THREAD_MULTIPLE;
+    } else if ((shmem_provided >= SHMEM_THREAD_SERIALIZED) && (mpi_provided >= MPI_THREAD_SERIALIZED)) {
+        *provided = MPI_THREAD_SERIALIZED;
+    } else if ((shmem_provided >= SHMEM_THREAD_FUNNELED) && (mpi_provided >= MPI_THREAD_FUNNELED)) {
+        *provided = MPI_THREAD_FUNNELED;
+    } else {
+        *provided = MPI_THREAD_SINGLE;
     }
 
     rc = PMPI_Win_create_keyval(MPI_WIN_NULL_COPY_FN, MPI_WIN_NULL_DELETE_FN, &rma2shmem_key, NULL);
