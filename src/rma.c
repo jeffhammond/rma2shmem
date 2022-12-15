6b8fc5eb087582032540df5e2d5b5c5107085985
Jeff Hammond <jeff.science@gmail.com>
2022-12-15 22:15:24 +0200

    outlining

--- src/rma.c
+++ src/rma.c
@@ -8,43 +8,30 @@ MPI_Win_get_attr(win, MPI_WIN_CREATE_FLAVOR, &create_kind, &flag), and
 MPI_Win_get_attr(win, MPI_WIN_MODEL, &memory_model, &flag)
 */
 
-bool RMA_Type_check(int origin_count, MPI_Datatype origin_datatype,
-                    int target_count, MPI_Datatype target_datatype,
-                    size_t * bytes)
+bool RMA_Type_check(int count, MPI_Datatype datatype, size_t * bytes)
 {
     int rc = MPI_SUCCESS;
+    *bytes = -1;
 
-    int origin_size;
-    rc = PMPI_Type_size(origin_datatype, &origin_size);
+    int size;
+    rc = PMPI_Type_size(datatype, &size);
     if (rc != MPI_SUCCESS) return false;
-    const size_t origin_bytes = origin_count * origin_size;
-
-    int target_size;
-    rc = PMPI_Type_size(target_datatype, &target_size);
-    if (rc != MPI_SUCCESS) return false;
-    const size_t target_bytes = target_count * target_size;
 
     MPI_Aint lb; // unused
 
     // The extent of a datatype is defined to be the span from the first byte to the last byte
     // occupied by entries in this datatype, rounded up to satisfy alignment requirements.
 
-    MPI_Aint origin_extent;
-    rc = PMPI_Type_get_extent(origin_datatype, &lb, &origin_extent);
+    MPI_Aint extent;
+    rc = PMPI_Type_get_extent(datatype, &lb, &extent);
     if (rc != MPI_SUCCESS) return false;
 
-    MPI_Aint target_extent;
-    rc = PMPI_Type_get_extent(target_datatype, &lb, &target_extent);
-    if (rc != MPI_SUCCESS) return false;
-
-    if ((origin_bytes  != target_bytes ) || (origin_extent != target_extent) ||
-        (origin_extent != origin_bytes ) || (target_extent != target_bytes )) {
-        RMA_Message("origin bytes=%zu extent=%zu target bytes=%zu extent=%zu\n",
-                     origin_bytes, origin_extent, target_bytes, target_extent);
+    if (extent != bytes ) {
+        RMA_Message("origin bytes=%zu extent=%zu\n", bytes, extent);
         return false;
     }
 
-    *bytes = origin_bytes;
+    *bytes = count * size;
     return true;
 }
 
@@ -54,25 +41,40 @@ int MPI_Put(RMA2SHMEM_CONST void *origin_addr, int origin_count, MPI_Datatype or
     rma2shmem_win_extras_s * extras;
     if ( RMA_Win_get_extras(win, &extras) && (extras->shmem_window) ) {
 
-        size_t bytes;
-        bool rx = RMA_Type_check(origin_count, origin_datatype, target_count, target_datatype, &bytes);
-        if (!rx) return MPI_ERR_TYPE;
+        size_t origin_bytes, target_bytes;
+        bool origin_contig = RMA_Type_check(origin_count, origin_datatype, &origin_bytes);
+        bool target_contig = RMA_Type_check(target_count, target_datatype, &target_bytes);
 
-        void * base = NULL;
-        rx = RMA_Win_get_base(win, &base);
-        if (rx != 0) return MPI_ERR_OTHER;
+        if (origin_contig) {
+            if (target_contig) {
 
-        int disp = 1;
-        rx = RMA_Win_get_disp_unit(win, &disp);
-        if (rx != 0) return MPI_ERR_OTHER;
+                void * base = NULL;
+                rx = RMA_Win_get_base(win, &base);
+                if (rx != 0) return MPI_ERR_OTHER;
 
-        void * dest = base + (ptrdiff_t)disp * (ptrdiff_t)target_disp;
+                int disp = 1;
+                rx = RMA_Win_get_disp_unit(win, &disp);
+                if (rx != 0) return MPI_ERR_OTHER;
 
-        // TODO remap rank to pe where appropriate
-        int pe = target_rank;
+                // map from (win,offset) to sheap offset relative to base
+                void * dest = base + (ptrdiff_t)disp * (ptrdiff_t)target_disp;
 
-        shmem_putmem_nbi(dest, origin_addr, bytes, pe);
-        return MPI_SUCCESS;
+                // TODO MPI comm / SHMEM team suppoort
+                const int pe = target_rank;
+
+                shmem_putmem_nbi(dest, origin_addr, bytes, pe);
+                return MPI_SUCCESS;
+
+            } else /* target non-contiguous */ {
+
+                return MPI_ERR_OTHER;
+
+                // TODO contiguous origin buffer
+
+            }
+        } else /* origin non-contiguous */ {
+
+        }
     }
     return PMPI_Put(origin_addr, origin_count, origin_datatype,
                     target_rank, target_disp, target_count, target_datatype,
