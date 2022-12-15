db90cfe9b9a78c600f85f3a92efe94e048d97d7a
Jeff Hammond <jeff.science@gmail.com>
2022-12-15 12:20:30 +0200

    disable unsupported

--- test/test_rma.c
+++ test/test_rma.c
@@ -25,6 +25,7 @@ int main(int argc, char * argv[])
 
     /* Allocate all the window memory on rank 0 */
     MPI_Win_allocate(winsize, sizeof(double), MPI_INFO_NULL, MPI_COMM_WORLD, &baseptr, &win);
+#if 0
     MPI_Win_lock_all(0, win);
 
     if (rank==0) {
@@ -63,6 +64,7 @@ int main(int argc, char * argv[])
     }
 
     MPI_Win_unlock_all(win);
+#endif
     MPI_Win_free(&win);
 
     MPI_Finalize();
