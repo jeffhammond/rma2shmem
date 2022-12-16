b25e66a1adec036a0fdd385fa61dd6e19ee85554
Jeff Hammond <jeff.science@gmail.com>
2022-12-16 11:41:47 +0200

    more tests

--- test/sandbox/datatypes.c
+++ test/sandbox/datatypes.c
@@ -6,14 +6,16 @@ int main(void)
     MPI_Init(NULL,NULL);
 
     MPI_Datatype dt;
-    MPI_Type_vector(100, 1, 2, MPI_INT, &dt);
+    MPI_Type_vector(100, 1, 2, MPI_BYTE, &dt);
 
     int s;
     MPI_Aint lb,e;
     MPI_Type_size(dt,&s);
     printf("type size %d\n",s);
     MPI_Type_get_extent(dt,&lb,&e);
-    printf("type extent %ld\n",e);
+    printf("type extent %ld, lb %ld\n",e,lb);
+    MPI_Type_get_true_extent(dt,&lb,&e);
+    printf("type true extent %ld, lb %ld\n",e,lb);
 
     MPI_Type_free(&dt);
 
